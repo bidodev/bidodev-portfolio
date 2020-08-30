27c3f1c9891f28624f79a4b5faeb49299f7f8d98
Claudinei Bido <bido.c@live.com>
2020-08-30 13:19:25 +0200

    projects data

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -3,27 +3,33 @@ import "./projects.component.styles.scss";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
+import DATA from "../../projects.json";
 
 const Projects = () => {
-  const PROJECTS_DATA = [{ id: "1" }, { id: "2" }, { id: "3" }];
+  const PROJECTS_DATA = DATA;
+  console.log(PROJECTS_DATA);
 
-  const CarouselItem = ({ id }) => {
+  const carouselSettings = {
+    showThumbs: false,
+    showStatus: false,
+    useKeyboardArrows: true,
+  };
+
+  const CarouselItem = ({ id, title, description, date, city, tecnologies }) => {
     return (
       <div>
         <div className="a">
           <div className="c">
-            <div className="tecnologies"></div>
-            <div className="title">
-                <p>Lorem ipsum dolor sit amet.</p>
-                <p>2020.09.10 BERLIN</p>
+            <div className="tecnologies">
+              {tecnologies.map((tecnologie) => <h3>{tecnologie}</h3>)}
             </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
-              quaerat impedit, eius libero ducimus quo rem corporis beatae eum
-              quia.
-            </p>
+            <div className="title">
+              <p>{title}</p>
+              <p>{date} {city}</p>
+            </div>
+            <p>{description}</p>
           </div>
-          <div className="d">ddddddddd</div>
+          <div className="d">{id}</div>
         </div>
       </div>
     );
@@ -35,9 +41,9 @@ const Projects = () => {
         <h2>Projects</h2>
       </div>
       <div className="container">
-        <Carousel>
-          {PROJECTS_DATA.map((project) => (
-            <CarouselItem key={project.id} id={project.id} />
+        <Carousel {...carouselSettings} className="presentation-mode">
+          {PROJECTS_DATA.map(({ ...project }) => (
+            <CarouselItem key={project.id} {...project} />
           ))}
         </Carousel>
       </div>
