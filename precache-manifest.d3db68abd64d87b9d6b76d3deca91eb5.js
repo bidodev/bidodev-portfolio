3182a97acc228636c8b6846827fedac72925845a
Claudinei Bido <bido.c@live.com>
2020-08-31 12:07:30 +0200

    Updates

---
+++ precache-manifest.d3db68abd64d87b9d6b76d3deca91eb5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "675786f9b91eb74edbbcb06d339140d1",
+    "url": "/bidodev-portfolio/index.html"
+  },
+  {
+    "revision": "0f6fb00027ed108cbb10",
+    "url": "/bidodev-portfolio/static/css/main.0340d9ad.chunk.css"
+  },
+  {
+    "revision": "1e12b29255a23685f3e8",
+    "url": "/bidodev-portfolio/static/js/2.938c8561.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bidodev-portfolio/static/js/2.938c8561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f6fb00027ed108cbb10",
+    "url": "/bidodev-portfolio/static/js/main.5bd68828.chunk.js"
+  },
+  {
+    "revision": "44be6552c82ea9a108fb",
+    "url": "/bidodev-portfolio/static/js/runtime-main.8d640e91.js"
+  },
+  {
+    "revision": "957647912e88e6ccfb883dc947e6ec49",
+    "url": "/bidodev-portfolio/static/media/1.95764791.svg"
+  },
+  {
+    "revision": "39d48ab3787fe77df60ed53fdbdf21a1",
+    "url": "/bidodev-portfolio/static/media/2.39d48ab3.svg"
+  },
+  {
+    "revision": "290490d9d6c575e909bc0403a78bf41d",
+    "url": "/bidodev-portfolio/static/media/3.290490d9.svg"
+  },
+  {
+    "revision": "176880ba2693d5ea975ddaaa3d0ac634",
+    "url": "/bidodev-portfolio/static/media/github.176880ba.svg"
+  },
+  {
+    "revision": "32cfe4c416329e3905f8e921a65f52bc",
+    "url": "/bidodev-portfolio/static/media/linkedin.32cfe4c4.svg"
+  },
+  {
+    "revision": "a4d14cdcfb1f945c6639e4611984a873",
+    "url": "/bidodev-portfolio/static/media/logo.a4d14cdc.svg"
+  }
+]);
